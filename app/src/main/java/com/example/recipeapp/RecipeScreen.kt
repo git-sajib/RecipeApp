fe1c97a81027f6b8c11012115028d4b07dc0b166
git-sajib <samiul.islam.unisel@gmail.com>
2024-07-26 16:58:31 +0600

    Preparing our UI for Navigation

--- app/src/main/java/com/example/recipeapp/RecipeScreen.kt
+++ app/src/main/java/com/example/recipeapp/RecipeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.recipeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -22,7 +23,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun RecipeScreen(modifier: Modifier = Modifier){
+fun RecipeScreen(modifier: Modifier = Modifier, navigateToDetail: (Category) -> Unit){
     val recipeViewModel: MainViewModel = viewModel()
     val viewState by recipeViewModel.categoriesState
     Box(modifier = Modifier.fillMaxSize()){
@@ -34,28 +35,29 @@ fun RecipeScreen(modifier: Modifier = Modifier){
             viewState.error != null -> {
                 Text(text = "Error Occurred")
             } else -> {
-                CategoryScreen(categories = viewState.list)
+                CategoryScreen(categories = viewState.list, navigateToDetail)
             }
         }
     }
 }
 
 @Composable
-fun CategoryScreen(categories: List<Category>){
+fun CategoryScreen(categories: List<Category>, navigateToDetail: (Category) -> Unit){
     LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
         items(categories){
             category -> 
-            CategoryItem(category = category)
+            CategoryItem(category = category, navigateToDetail)
         }
     }
 }
 
 //How each items looks like
 @Composable
-fun CategoryItem(category: Category){
+fun CategoryItem(category: Category, navigateToDetail: (Category) -> Unit){
     Column(modifier = Modifier
         .padding(8.dp)
-        .fillMaxSize(),
+        .fillMaxSize()
+        .clickable { navigateToDetail(category) },
         horizontalAlignment = Alignment.CenterHorizontally)
     {
         Image(
